8567e6ca9606204cdc87b9a3ce12c7fb726ef5d3
khegazy <kareemhegz@gmail.com>
2018-04-12 13:29:32 -0700

    Fix index bug

--- centerImg.py
+++ centerImg.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def center_image(image, centerR, centerC, roi):
   roiRange = roi//2
-  assert ((roiRange+centerR < image.shape[0]) and (centerR-roiRange >= 0)),\
+  assert ((roiRange+centerR+1 < image.shape[0]) and (centerR-roiRange >= 0)),\
       "ERROR: row ROI {} is out of range for image of size {} and center row {}".format(
-      roi, image.shape, centerR)
-  assert ((roiRange+centerC < image.shape[1]) and (centerC-roiRange >= 0)),\
+      roi+1, image.shape, centerR)
+  assert ((roiRange+centerC+1 < image.shape[1]) and (centerC-roiRange >= 0)),\
       "ERROR: col ROI {} is out of range for image of size {} and center col {}".format(
-      roi, image.shape, centerR)
+      roi+1, image.shape, centerR)
 
   return image[centerR-roiRange:centerR+roiRange+1,centerC-roiRange:centerC+roiRange+1]
