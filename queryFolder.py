80713d46815f00f2f84ff65202723748d206bd1b
khegazy <kareemhegz@gmail.com>
2018-05-30 17:36:14 -0700

    Bug fix.

--- queryFolder.py
+++ queryFolder.py
@@ -1,5 +1,5 @@
 import glob
 
 def query_folder(folder, fileExtention, loadedFiles):
-  folderFiles = glob.glob(folder + "/" + fileExtention)
+  folderFiles = glob.glob(folder + "/*" + fileExtention)
   return [fl for fl in folderFiles if fl not in loadedFiles]
